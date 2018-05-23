a245c35a62d3794fb4268975c05ae90f012011a1
BrainStone <yannick@tedworld.de>
2018-05-23 21:36:23 +0200

    Remove the plugin for the TS Client

--- install_pterodactyl.sh
+++ install_pterodactyl.sh
@@ -9,6 +9,7 @@ cd /mnt/server
 
 curl -sS https://www.sinusbot.com/pre/sinusbot-0.13.37-f7e9ece.tar.bz2 | tar xj
 chmod +x sinusbot
+rm -r plugin
 
 sed -i "s|^TS3Path.*|TS3Path = \"/opt/sinusbot/TeamSpeak3-Client-linux_amd64/ts3client_linux_amd64\"|g" config.ini.dist
 echo 'YoutubeDLPath = "/usr/local/bin/youtube-dl"' >> config.ini.dist
